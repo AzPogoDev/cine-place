e61ffe9647907ca7a226eda3d06e431ca5cbe872
AzPogoDev <henricousin07@gmail.Com>
2022-06-10 12:28:38 +0200

    Missing files

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { DataService } from '../data.service';
 
+
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss'],
 })
 export class Tab3Page {
+
   cinemas = [];
 
   constructor(private dataService: DataService) {}
